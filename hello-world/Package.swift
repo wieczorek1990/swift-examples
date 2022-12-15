0661ce35ac3e5f9b1ba7032873aee51dc1f19b75
Łukasz Wieczorek <luke@soiree.tech>
2022-12-15 02:13:34 +0100

    Add hello-world sample.

---
+++ hello-world/Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "hello-world",
+    dependencies: [],
+    targets: [
+        .executableTarget(
+            name: "hello-world",
+            dependencies: []),
+    ]
+)
